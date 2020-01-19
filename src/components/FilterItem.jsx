fa606e45e2c84ffbcc4a663a915fb847503ef979
Carlos <carlosgarcia_97gr@hotmail.com>
2020-01-19 12:02:51 -0500

    cambios en los item

--- src/components/FilterItem.jsx
+++ src/components/FilterItem.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class FilterItem extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            isActive:false
-        }
-    }
-    
-    render() {
-        return (
-        <div className="filterItem">{this.props.name||'Filtro'}</div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+  }
+
+  render() {
+    return <div className=" filterItem">{this.props.name || "Filtro"}</div>;
+  }
 }
